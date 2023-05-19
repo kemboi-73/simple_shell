40ce7b60f0788d351acc848adfe69d5a6f99c34e
kemboi-73 <kemboimichael52@gmail.com>
2023-05-19 09:55:38 -0700

    the main header file

--- shell.h
+++ shell.h
@@ -1,2 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int stat(const char *pathname, struct stat *statbuf);
+void execmd(char **argv);
+char *get_location(char *command);
+
+#endif
+
