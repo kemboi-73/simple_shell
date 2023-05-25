f4d318f216e0c483293d73475ce7a9390fab37e7
kemboi-73 <kemboimichael52@gmail.com>
2023-05-25 09:59:16 -0700

    testing

--- free.c
+++ free.c
@@ -1,29 +1,15 @@
 #include "shell.h"
-
-/**
- * freearg - function that frees the memory allocated for an array of strings
- * @arg: array of pointers to character
- */
-void freearg(char **arg) {
-    int i;
-
-    for (i = 0; arg[i] != NULL; i++) {
-        free(arg[i]);
-    }
-
-
-    free(arg);
-}
-
 /**
  * free_all - function that frees all allocated memory
- * @buffer: buffer where getline stores the commands
+ * that needed to be freed
+ * @buffer: buffer where getline stock the commands
  * @p: pointer to character
- * @arg: array of pointers to character
+ * @arg: arry of pointers to character
+ * Return: nothing
  */
-void free_all(char *buffer, char *p, char **arg) {
-    free(buffer);
-    free(p);
-    freearg(arg);
+void free_all(char *buffer, char *p, char **arg)
+{
+	free(buffer);
+	free(p);
+	freearg(arg);
 }
-
