f5fbac04477fd7729def9b485182a28183df6590
kemboi-73 <kemboimichael52@gmail.com>
2023-05-26 05:02:13 -0700

    testing_shell

--- command_exemd.c
+++ command_exemd.c
@@ -1,22 +1,26 @@
 #include "shell.h"
 
 /**
- * execmd - handles the command line
- * @argv: the argument itself
+ * execmd - function to handle the command line
+ * @argv: argument
  */
 void execmd(char **argv)
 {
-	char *command = NULL, *actual_cmd = NULL;
+    char *command = NULL, *actual_cmd = NULL;
 
-	if (argv)
-	{
-	command = argv[0];
-		actual_cmd = get_location(command);
+    if (argv)
+    {
+        command = argv[0];
+        actual_cmd = get_location(command);
 
-if (execve(actual_cmd, argv, NULL) == -1)
-	{
-perror("Oops! An error occurred.\n");
-	}
-	}
+        if (strcmp(command, "exit") == 0) {
+            exit(0);
+        }
+
+        if (execve(actual_cmd, argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
+    }
 }
 
