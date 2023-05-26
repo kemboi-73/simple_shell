e4d8ceed4a9f444768ca17f5f409c01f1b0044a2
kemboi-73 <kemboimichael52@gmail.com>
2023-05-26 06:08:58 -0700

    testing

--- command_exemd.c
+++ command_exemd.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "shell.h"
 
 /**
@@ -6,22 +9,20 @@
  */
 void execmd(char **argv)
 {
-    char *command = NULL, *actual_cmd = NULL;
+char *command = NULL, *actual_cmd = NULL;
 
     if (argv)
     {
-        command = argv[0];
-        actual_cmd = get_location(command);
-
-        if (strcmp(command, "exit") == 0)
-        {
-            exit(0);
-        }
-
-        if (execve(actual_cmd, argv, NULL) == -1)
-        {
-            perror("Oops! An error occurred.\n");
-        }
-    }
+	command = argv[0];
+	actual_cmd = get_location(command);
+if (strcmp(command, "exit") == 0)
+	{
+	exit(0);
+	}
+if (execve(actual_cmd, argv, NULL) == -1)
+	{
+	perror("Oops! An error occurred.\n");
+}
+	}
 }
 
