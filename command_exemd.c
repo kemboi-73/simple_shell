f1d07ae8202440c882153635ea0007646fbb174d
kemboi-73 <kemboimichael52@gmail.com>
2023-05-27 09:16:23 -0700

    shell

--- command_exemd.c
+++ command_exemd.c
@@ -3,32 +3,19 @@
 /**
  * exemd - function to handle the command line
  * @argv: argument
-*/
+ */
 void exemd(char **argv)
 {
-char *command = argv[0];
-char *actual_cmd = NULL;
-	pid_t pid;
+	char *command = NULL, *actual_cmd = NULL;
 
-	if (strcmp(command, "exit") == 0)
-			exit(0);
 	if (argv)
-{
-	command = argv[0];
-	actual_cmd = get_location(command);
+	{
+		command = argv[0];
+		actual_cmd = get_location(command);
 
-		pid = fork();
-
-		if (pid < 0)
-		write(2, "fork", 4);
-else if (pid == 0)
-{
-if (execve(actual_cmd, argv, NULL) == -1)
-{
-	perror("Oops! An error occurred.\n");
-	exit(1);
+		if (execve(actual_cmd, argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
 	}
-exit(0);
-	}
-}
 }
